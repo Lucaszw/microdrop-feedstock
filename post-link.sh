e3707cb27354f6402525a04aa3327666f44789eb
lucaszw <lucas.zeer@gmail.com>
2017-11-30 10:42:19 -0500

    comment post-link commands for now

--- post-link.sh
+++ post-link.sh
@@ -1,3 +1,3 @@
 source bin/activate
-npm --prefix share/microdrop-3/feedstock install
-gulp --cwd share/microdrop-3/feedstock conda:post-link
+# npm --prefix share/microdrop-3/feedstock install
+# gulp --cwd share/microdrop-3/feedstock conda:post-link
