f0fd7613dbe2d8ba54add4f63cbc378469f6c3ff
lucaszw <lucas.zeer@gmail.com>
2017-11-30 11:14:46 -0500

    fix source activation to be proper directory

--- post-link.sh
+++ post-link.sh
@@ -1,5 +1,3 @@
-ls
-pwd
-source bin/activate
-# npm --prefix share/microdrop-3/feedstock install
-# gulp --cwd share/microdrop-3/feedstock conda:post-link
+source activate
+npm --prefix share/microdrop-3/feedstock install
+gulp --cwd share/microdrop-3/feedstock conda:post-link
